b3aa719bb64a83ec2df1cd93ae7d14685e886930
Pavlo <warty24@gmail.com>
2022-08-30 11:44:28 +0300

    added restAssured support, method to get config file from source

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,5 +1,6 @@
 package base;
 
+import config.AppConfig;
 import config.ConfigManager;
 import config.UiSettings;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -7,8 +8,10 @@ import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.*;
 import org.testng.annotations.*;
-
+import io.restassured.response.Response;
 import java.time.Duration;
+
+import static io.restassured.RestAssured.*;
 
 public class BaseTest {
     @Getter
@@ -18,16 +21,25 @@ public class BaseTest {
     @BeforeSuite
     public void beforeSuite() {
         settings = ConfigManager.getConfiguration().getUiSettings();
+        if(settings == null) {
+            settings = getRemoteJSON().getUiSettings();
+        }
+
         startChrome();
     }
     @AfterSuite
     public void afterSuite() {
-  //      driver.quit();
+        driver.quit();
     }
 
     private void startChrome() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+    }
+    AppConfig getRemoteJSON() {
+        String configFileAddress = "";
+        Response resp = get(configFileAddress);
+        return resp.as(AppConfig.class);
     }
 }
